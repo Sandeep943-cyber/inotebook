7485ead0ed9dd31a41a2e6c6aa637cff2719fd25
Sandeep Joshi <joshisandeep943@gmail.com>
2023-01-31 12:59:38 +0530

    Hello World

---
+++ backend/db.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+const mongoURI="mongodb+srv://Sandeep943:Iw%40nttodrink1234@cluster0.q3k33xd.mongodb.net/test"
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to mongo succesfully");
+    })
+}
+
+module.exports=connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,21 @@
+const connectToMongo=require('./db');  
+const express = require('express')
+
+connectToMongo(); 
+const app = express()
+const port = 5000
+
+app.use(express.json())
+
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.get('/', (req, res) => {
+  res.send('Hello Sandy!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
   
